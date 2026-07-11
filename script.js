e9cd028966485a50825bc03757ca12da45561772
piyushhchaudhary <chaudharyypiyushh@gmail.com>
2026-07-12 00:50:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,3 +32,43 @@ function openEnvelope() {
     }, 800);
 
 }
+const message = `Dear Smita ❤️,
+
+Warning! 😄
+
+Agar tum ye letter padh rahi ho,
+to iska matlab tum meri chhoti si surprise journey ka hissa ban chuki ho.
+
+Ab promise karo...
+Aakhri page tak bina skip kiye dekhogi. 🤭💕
+
+Aur haan...
+Beech me hasna, sharmaana aur smile karna allowed hai. 😊🌸
+
+Ready?
+
+Let's begin... 🦋`;
+
+let index = 0;
+
+function typeLetter() {
+
+    const text = document.getElementById("letterText");
+
+    text.innerHTML = "";
+
+    index = 0;
+
+    const typing = setInterval(() => {
+
+        text.innerHTML += message.charAt(index);
+
+        index++;
+
+        if (index >= message.length) {
+            clearInterval(typing);
+        }
+
+    }, 40);
+
+}
