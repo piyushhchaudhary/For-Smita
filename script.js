4efd318e3b005e85755d0b2adeece23bdbb0fca7
piyushhchaudhary <chaudharyypiyushh@gmail.com>
2026-07-11 23:43:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,9 +19,9 @@ function openEnvelope(){
 
     setTimeout(function(){
 
-        envelope.style.display = "none";
-        letter.style.display = "block";
+    envelope.style.display = "none";
+    letter.style.display = "block";
 
-    },800);
+    typeLetter();
 
-}
+},800);
