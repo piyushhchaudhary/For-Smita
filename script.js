07edd94554c8afbb4342cfbcf0bfe4bdcb5b6664
piyushhchaudhary <chaudharyypiyushh@gmail.com>
2026-07-12 00:01:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,26 +5,13 @@ window.onload = function () {
     setTimeout(function () {
 
         welcome.style.opacity = "0";
-document.getElementById("scene2").style.display = "flex";
- // welcome.style.display = "none";
+
+        document.getElementById("scene2").style.display = "flex";
+
     }, 3000);
 
 };
-function openEnvelope(){
 
-    const envelope = document.getElementById("envelope");
-    const letter = document.getElementById("letter");
-
-    envelope.classList.add("open");
-
-    setTimeout(function(){
-
-    envelope.style.display = "none";
-    letter.style.display = "block";
-
-    typeLetter();
-
-},800);
 const message = `Dear Smita ❤️,
 
 Warning! 😄
@@ -44,22 +31,40 @@ Let's begin... 🦋`;
 
 let index = 0;
 
-function typeLetter(){
+function typeLetter() {
 
     const text = document.getElementById("letterText");
+    text.innerHTML = "";
 
-    const typing = setInterval(function(){
+    const typing = setInterval(function () {
 
         text.innerHTML += message.charAt(index);
 
         index++;
 
-        if(index >= message.length){
-
+        if (index >= message.length) {
             clearInterval(typing);
-
         }
 
-    },40);
+    }, 40);
+
+}
+
+function openEnvelope() {
+
+    const envelope = document.getElementById("envelope");
+    const letter = document.getElementById("letter");
+
+    envelope.classList.add("open");
+
+    setTimeout(function () {
+
+        envelope.style.display = "none";
+        letter.style.display = "block";
+
+        index = 0;
+        typeLetter();
+
+    }, 800);
 
 }
